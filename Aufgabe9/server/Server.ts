f1172d172885f86c5a582c58e547bb073a742e05
Bastian-C <b.culig@t-online.de>
2019-05-26 16:59:11 +0200

    Draft 1

--- Aufgabe9/server/Server.ts
+++ Aufgabe9/server/Server.ts
@@ -1,34 +1,72 @@
-import * as Http from "http"; // Importiere alle Inhalte aus "http" als Typ Http.
+/**
+ * Simple server managing between client and database
+ * @author: Jirka Dell'Oro-Friedl
+ * @adapted: Lukas Scheuerle
+ */
+
+import * as Http from "http";
 import * as Url from "url";
+import * as Database from "./Database";
 
-{										// Erstelle Namespace "L05 Server"
-	console.log("Starting server");							// "Starting server" wird auf der Console ausgegeben.
-	let port: number = Number(process.env.PORT);			// Variable "port" vom typ number wird erstellt und der Port zugewiesen, auf welchen der Server hören soll.
-	if (!port)												// Wenn port nicht der Fall sein soll, führe nächste Zeile aus.
-		port = 8100;										// port wird zu 8100
+console.log("Server starting");
 
-	let server: Http.Server = Http.createServer();			// Variable "server" vom Typ Http.Server erstellen und dieser Variable wird die Funktion createServer() zugewiesen. createServer() befindet sich in dem importierten Code.
-	server.addListener("request", handleRequest);			// "server" wird ein listener hinzugefügt. Der Listener hört auf einen request und führt handleRequest aus.
-	server.addListener("listening", handleListen);			// "server" wird ein listener hinzugefügt. Der Listener hört auf listening und führt handleListen aus.
-	server.listen(port);									// Listener wird erstellt. Er hört auf die oben definierte Variable "port".
+let port: number = Number(process.env.PORT);
+if (!port)
+    port = 8100;
 
-	function handleListen(): void {							// Funktion handleListen wird definiert. Übergabeparameter: None. Rückgabewert: None. 
-		console.log("Listening");							// "Listening" wird an die Console ausgegeben.
-	}														// Funktion handleListen schließen
-
-	function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { 	// Funktion handleRequest wird definiert. Übergabeparameter: _request und _response. Rückgabewert: None. 
-		console.log("I hear voices!"); 						// "I hear voices!" wird an die Console ausgegeben.
-		_response.setHeader("content-type", "text/html; charset=utf-8");							// Header-Werte für ein Header-Objekt wird festgelegt. Preexistierender Header-Wert wird ersetzt 
-		_response.setHeader("Access-Control-Allow-Origin", "*");									// Header-Wert für Access Control wird festgelet. "*" lässt jeden Origin zu (anstelle von <origin> für spezifische Origins oder "null" für keinen möglichen Origin)
-
-
-		_response.write("<h2>Order Recieved:</h2>");
-		let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-		for (let key in url.query)
-		_response.write("<p>" + key + url.query[key] + "</p> <br>");
+let server: Http.Server = Http.createServer();
+server.addListener("listening", handleListen);
+server.addListener("request", handleRequest);
+server.listen(port);
 
 
 
-		_response.end();									// Der Server erhält eine Bestätigung, dass der request vollständig ist.
-	}														// Funktion handleRequest schließen
-}															// Namespace "L05 Server" schließen
+function handleListen(): void {
+    console.log("Listening on port: " + port);
+}
+
+function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    console.log("Request received");
+
+    let query: AssocStringString = <AssocStringString> Url.parse(_request.url, true).query;
+    let command: string = query["command"];
+    let matrikel: string = query["matrikel"];
+    switch (command) {
+        case "insert":
+            let student: StudentData = {
+                name: query["name"],
+                firstname: query["firstname"],
+                matrikel: parseInt(query["matrikel"])
+            };
+            Database.insert(student);
+            respond(_response, "storing data");
+            break;
+        case "refresh":
+            Database.findAll(findCallback);
+            break;
+            case "search":
+                for(let key in query){
+                    if(key == "matrikel"){
+                        Database.MatrikelSearch(Number(matrikel), findCallback);
+                    }
+                }
+                break;
+        default:
+            respond(_response, "unknown command: " + command);
+            break;
+            
+    }
+
+    // findCallback is an inner function so that _response is in scope
+    function findCallback(json: string): void {
+        respond(_response, json);
+    }
+}
+
+function respond(_response: Http.ServerResponse, _text: string): void {
+    //console.log("Preparing response: " + _text);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write(_text);
+    _response.end();
+}
